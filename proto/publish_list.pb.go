95773796855db4d0afbb021d638b3fcb2b975285
Nathan <wenzhu_work@163.com>
2023-01-29 19:45:51 +0800

    1. 修复投稿接口存在的bug 2. 为.pb.go文件增加必要tag

--- proto/publish_list.pb.go
+++ proto/publish_list.pb.go
@@ -25,8 +25,8 @@ type DouyinPublishListRequest struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	UserId *int64  `protobuf:"varint,1,req,name=user_id,json=userId" json:"user_id,omitempty"` // 用户id
-	Token  *string `protobuf:"bytes,2,req,name=token" json:"token,omitempty"`                  // 用户鉴权token
+	UserId *int64  `protobuf:"varint,1,req,name=user_id,json=userId" json:"user_id,omitempty" form:"user_id"` // 用户id
+	Token  *string `protobuf:"bytes,2,req,name=token" json:"token,omitempty" form:"token"`                  // 用户鉴权token
 }
 
 func (x *DouyinPublishListRequest) Reset() {
