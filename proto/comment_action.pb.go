6c632e2c5f26efeea2dd5fd7a36109fed4103af2
Nathan <wenzhu_work@163.com>
2023-01-29 19:55:50 +0800

    修改评论接口中可能引发bug的部分代码

--- proto/comment_action.pb.go
+++ proto/comment_action.pb.go
@@ -25,11 +25,11 @@ type DouyinCommentActionRequest struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Token       *string `protobuf:"bytes,1,req,name=token" json:"token,omitempty"`                                // 用户鉴权token
-	VideoId     *int64  `protobuf:"varint,2,req,name=video_id,json=videoId" json:"video_id,omitempty"`            // 视频id
-	ActionType  *int32  `protobuf:"varint,3,req,name=action_type,json=actionType" json:"action_type,omitempty"`   // 1-发布评论，2-删除评论
-	CommentText *string `protobuf:"bytes,4,opt,name=comment_text,json=commentText" json:"comment_text,omitempty"` // 用户填写的评论内容，在action_type=1的时候使用
-	CommentId   *int64  `protobuf:"varint,5,opt,name=comment_id,json=commentId" json:"comment_id,omitempty"`      // 要删除的评论id，在action_type=2的时候使用
+	Token       *string `protobuf:"bytes,1,req,name=token" json:"token,omitempty" form:"token" binding:"required"`                                    // 用户鉴权token
+	VideoId     *int64  `protobuf:"varint,2,req,name=video_id,json=videoId" json:"video_id,omitempty" form:"video_id" binding:"required"`             // 视频id
+	ActionType  *int32  `protobuf:"varint,3,req,name=action_type,json=actionType" json:"action_type,omitempty" form:"action_type" binding:"required"` // 1-发布评论，2-删除评论
+	CommentText *string `protobuf:"bytes,4,opt,name=comment_text,json=commentText" json:"comment_text,omitempty" form:"comment_text"`                 // 用户填写的评论内容，在action_type=1的时候使用
+	CommentId   *int64  `protobuf:"varint,5,opt,name=comment_id,json=commentId" json:"comment_id,omitempty" form:"comment_id"`                        // 要删除的评论id，在action_type=2的时候使用
 }
 
 func (x *DouyinCommentActionRequest) Reset() {
